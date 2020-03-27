45969e5900c75bcf19ddfd9d8382a5dfc7da808e
alexis villaraza <alexis.borja@gmail.com>
2020-03-27 09:23:51 -0700

    got login to work

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,13 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
     useNullAsDefault: true,
+    connection: { filename: './database/auth.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
